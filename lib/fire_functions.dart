e08f835ad9a62651ddf6af86a839df772e9176ef
Kevin Renner <krenner529@gmail.com>
2018-04-20 16:16:58 -0500

    Fixing pubspec and removing unused function

--- lib/fire_functions.dart
+++ lib/fire_functions.dart
@@ -6,11 +6,6 @@ class FireFunctions {
   static const MethodChannel _channel =
       const MethodChannel('fire_functions');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static Future callFunction(String functionName, Map<String, dynamic> data) async {
     final result = await _channel.invokeMethod(functionName, data);
     return result;
